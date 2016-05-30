890acf782ca82cf67884899de183581922baf9a6
杜晓宇 <noliar@qq.com>
2016-05-30 14:55:21 +0800

    Adding Microsoft.Extensions.Configuration.CommandLine

--- MyMvc/Program.cs
+++ MyMvc/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace MyMvc
 {
@@ -11,9 +12,12 @@ namespace MyMvc
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
